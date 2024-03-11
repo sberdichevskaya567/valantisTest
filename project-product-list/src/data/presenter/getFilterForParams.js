0e63981fd4378ba44af90e18ea5e52d14d552915
Aleksandra <baiipstomsk20@gmail.com>
2024-03-11 21:30:34 +0700

    fix

--- project-product-list/src/data/presenter/getFilterForParams.js
+++
@@ -1,9 +0,0 @@
-import { Mutation } from "../mutation/index.js"
-
-const getIdsFromSelectedFilter = (params) => {
-
-    const { data: idsFromSelectedFilter } = Mutation('filter', params)
-    return { idsFromSelectedFilter }
-}
-
-export { getIdsFromSelectedFilter }
